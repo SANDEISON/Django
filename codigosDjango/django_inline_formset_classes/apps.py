b79005f7bbf510400c0ca2d14259c5c484891758
SANDEISON <sandeisonfernandes@gmail.com>
2024-02-25 06:01:58 -0300

    Adicionando a estrutura basica de um projeto em Django, com um app criado

---
+++ codigosDjango/django_inline_formset_classes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoInlineFormsetClassesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_inline_formset_classes'
